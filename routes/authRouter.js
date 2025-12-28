95f9c640320c5ece86104f61a128444d569567ee
v.karakai <v.karakai@pin-up.tech>
2025-12-28 18:31:42 +0200

    Add login functionality with JWT authentication

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
 import validateBody from "../helpers/validateBody.js";
 import { registerSchema } from "../schemas/authSchemas.js";
+import { login } from "../controllers/authLoginController.js";
 import { register } from "../controllers/authController.js";
+import { loginSchema } from "../schemas/authSchemas.js";
+
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validateBody(registerSchema), register);
+authRouter.post("/login", validateBody(loginSchema), login);
 
 export default authRouter;
