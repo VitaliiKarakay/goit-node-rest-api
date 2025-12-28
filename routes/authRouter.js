493433fe9fa4510efe6c786280a3edd61006b05c
v.karakai <v.karakai@pin-up.tech>
2025-12-28 18:39:45 +0200

    Add logout functionality to authentication flow

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,7 +1,9 @@
 import express from "express";
 import validateBody from "../helpers/validateBody.js";
 import { registerSchema } from "../schemas/authSchemas.js";
+import authenticate from "../middlewares/authenticate.js";
 import { login } from "../controllers/authLoginController.js";
+import { logout } from "../controllers/authLogoutController.js";
 import { register } from "../controllers/authController.js";
 import { loginSchema } from "../schemas/authSchemas.js";
 
@@ -10,5 +12,6 @@ const authRouter = express.Router();
 
 authRouter.post("/register", validateBody(registerSchema), register);
 authRouter.post("/login", validateBody(loginSchema), login);
+authRouter.post("/logout", authenticate, logout);
 
 export default authRouter;
